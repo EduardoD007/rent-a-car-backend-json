51afed248ba6ca71b35dc682f6ba4f5a0c09f9ba
Eduardo Dias <edufreitas205@gmail.com>
2025-04-28 19:37:36 -0300

    setup header 4

--- app.js
+++ app.js
@@ -6,21 +6,25 @@ const app = express();
 
 //app.use(cors());
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin : https://rent-a-car-frontend-v2.onrender.com")
-  res.header(
-    "Access-Control-Allow-Headers",
-    "content-type"
-  )
-  if (req.method === "OPTIONS") {
-    res.header(
-      "Access-Control-Allow-Methods",
-      "POST, PUT, PATCH, GET, DELETE"
-    )
-    return res.status(200).json({})
-  }
-  next()
-})
+
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://rent-a-car-frontend-v2.onrender.com');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 routes(app);
 

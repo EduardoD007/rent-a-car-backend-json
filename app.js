1b29eda710645a82162c2ee07142c6cf10da460d
Eduardo Dias <edufreitas205@gmail.com>
2025-04-28 18:47:45 -0300

    setup header 2

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept Authorization"
+    "content-type"
   )
   if (req.method === "OPTIONS") {
     res.header(

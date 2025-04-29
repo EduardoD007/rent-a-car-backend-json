6343f3c23fea6f824a85fd91c84f281f1e75c75d
Eduardo Dias <edufreitas205@gmail.com>
2025-04-29 07:18:39 -0300

    setup cors 9

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const routes = require('./app/routes');
-const cors = require('cors')
+var cors = require('cors')
+
+app.use(cors());
 
 const app = express();
 
-
+routes(app);
 
 
 app.use(function (req, res, next) {
@@ -16,15 +18,15 @@ app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
   // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader("Access-Control-Allow-Headers", "application/x-www-form-urlencoded",
+    "Origin, X-Requested-Width, Content-Type, Accept, Authorization");
 
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
   res.setHeader('Access-Control-Allow-Credentials', true);
 
-  app.use(cors());
+  app.delete('/pedidos/:pedidoId/:carroId')
 
-  routes(app);
   // Pass to next layer of middleware
   next();
 

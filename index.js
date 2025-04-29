da0a19675e60878d4d4ae0be55ebf2f2f9692195
Eduardo Dias <edufreitas205@gmail.com>
2025-04-28 22:44:09 -0300

    process.env.PORT

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const cors = require('cors')
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://rent-a-car-frontend-v2.onrender.com'
+}));
 
 
 /* app.use(function (req, res, next) {
